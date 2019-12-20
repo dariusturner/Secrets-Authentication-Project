4703f5017954ba8bdf2459f0fbe8cc9a4f314359
Darius Turner <dariusot731@gmail.com>
2019-12-20 05:23:56 -0500

    Level 4 - bycrpt added and fixed typo in account not found page

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 const port = 3000;
@@ -22,8 +23,6 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-
-
 const User = new mongoose.model("User", userSchema);
 
 app.get("/", function(req, res){
@@ -35,16 +34,23 @@ app.get("/login", function(req, res){
 });
 
 app.post("/login", function(req, res){
-  User.findOne({email: req.body.username}, function(err, foundUser){
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email: username}, function(err, foundUser){
     if (err) {
       console.log(err);
     } else {
       if (foundUser){
-        if (foundUser.password === md5(req.body.password)) {
-          res.render("secrets");
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if (result === true) {
+            res.render("secrets");
+          } else {
+            res.send("<h1>404</h1><p>Sorry your email or password was incorrect.</p><a href='/'>Try Again</a><style>h1{font-size:8em;text-align:center;margin-bottom: 10px;}p{font-size:2em;text-align:center;}a{font-size:2em;text-align:center;display: block;color: blue;text-decoration: none;}a:hover{color:#33bbff;}</style>");
+          }
+        });
       } else {
-        res.send("<h1>404</h1><p>Sorry that account does is not registered for this site.</p><a href='/'>Try Again</a><style>h1{font-size:8em;text-align:center;margin-bottom: 10px;}p{font-size:2em;text-align:center;}a{font-size:2em;text-align:center;display: block;color: blue;text-decoration: none;}a:hover{color:#33bbff;}</style>");
+        res.send("<h1>404</h1><p>Sorry that account is not registered for this site.</p><a href='/'>Try Again</a><style>h1{font-size:8em;text-align:center;margin-bottom: 10px;}p{font-size:2em;text-align:center;}a{font-size:2em;text-align:center;display: block;color: blue;text-decoration: none;}a:hover{color:#33bbff;}</style>");
       }
     }
   });
@@ -55,17 +61,23 @@ app.get("/register", function(req, res){
 });
 
 app.post("/register", function(req, res){
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
+  const username = req.body.username;
+  const password = req.body.password;
+
+  bcrypt.hash(password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: username,
+      password: hash
+    });
+    newUser.save(function(err){
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
   });
-  newUser.save(function(err){
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
-  });
+
 });
 
 app.get("/logout", function(req, res){
