689c7770cd3bdfdf5e46c6efb9217e0c4e015adc
Darius Turner <dariusot731@gmail.com>
2019-12-19 19:30:44 -0500

    Add Mongoose and set up the register and login pages to validate accounts

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require('mongoose');
 
 const app = express();
 const port = 3000;
@@ -12,6 +13,15 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true});
+
+const userSchema = {
+  email: String,
+  password: String
+};
+
+const User = new mongoose.model("User", userSchema);
+
 app.get("/", function(req, res){
   res.render("home");
 });
@@ -20,10 +30,35 @@ app.get("/login", function(req, res){
   res.render("login");
 });
 
+app.post("/login", function(req, res){
+  User.find({username: req.body.username, password: req.body.password}, function(err, foundUser){
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser){
+        res.render("secrets");
+      }
+    }
+  });
+});
+
 app.get("/register", function(req, res){
   res.render("register");
 });
 
+app.post("/register", function(req, res){
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+  newUser.save(function(err){
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
+  });
+});
 
 
 app.listen(port, function(){
