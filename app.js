b6a21012a8b156e6b8ae32570dc18837bba669a5
Darius Turner <dariusot731@gmail.com>
2019-12-20 04:43:02 -0500

    Level 3 - Hashing Passwords added

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 //jshint esversion:6
 require('dotenv').config();
-const express = require("express");
-const bodyParser = require("body-parser");
-const ejs = require("ejs");
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 const app = express();
 const port = 3000;
@@ -22,7 +22,7 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+
 
 const User = new mongoose.model("User", userSchema);
 
@@ -40,7 +40,7 @@ app.post("/login", function(req, res){
       console.log(err);
     } else {
       if (foundUser){
-        if (foundUser.password === req.body.password) {
+        if (foundUser.password === md5(req.body.password)) {
           res.render("secrets");
         }
       } else {
@@ -57,7 +57,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res){
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
   newUser.save(function(err){
     if (err) {
